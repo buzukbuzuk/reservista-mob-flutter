a082f1c4c43466aab19628474270bc643ab9accf
aidostt <leket1999@gmail.com>
2024-06-10 21:56:00 +0500

    added connection with sign in and sign up endpooints of backend server

--- lib/pages/authentic/authentic_model.dart
+++ lib/pages/authentic/authentic_model.dart
@@ -9,6 +9,7 @@ import 'package:flutter/scheduler.dart';
 import 'package:flutter_animate/flutter_animate.dart';
 import 'package:google_fonts/google_fonts.dart';
 import 'package:provider/provider.dart';
+import 'package:http/http.dart' as http;
 
 class AuthenticModel extends FlutterFlowModel<AuthenticWidget> {
   ///  State fields for stateful widgets in this page.
@@ -43,6 +44,79 @@ class AuthenticModel extends FlutterFlowModel<AuthenticWidget> {
   TextEditingController? passwordTextController;
   late bool passwordVisibility;
   String? Function(BuildContext, String?)? passwordTextControllerValidator;
+
+  Future<void> signUp(BuildContext context, String name, String surname, String phone, String email, String password) async {
+    final url = Uri.parse('http://185.146.1.28:8000/api/auth/sign-up');
+    final response = await http.post(
+      url,
+      headers: <String, String>{
+        'Content-Type': 'application/json; charset=UTF-8',
+      },
+      body: jsonEncode({
+        'name': name,
+        'surname': surname,
+        'phone': phone,
+        'email': email,
+        'password': password,
+      }),
+    );
+
+    if (response.statusCode == 201) {
+      // Navigate to the home page
+      context.pushNamed('Activation');
+    } else if (response.statusCode == 400) {
+      _showErrorPopup(context, 'Invalid input arguments');
+    } else if (response.statusCode == 500) {
+      _showErrorPopup(context, 'Something went wrong..');
+    }
+  }
+
+  Future<void> signIn(BuildContext context, String email, String password) async {
+    final url = Uri.parse('http://185.146.1.28:8000/api/auth/sign-in');
+    final response = await http.post(
+      url,
+      headers: <String, String>{
+        'Content-Type': 'application/json; charset=UTF-8',
+      },
+      body: jsonEncode({
+        'email': email,
+        'password': password,
+      }),
+    );
+
+    if (response.statusCode == 200) {
+      // Navigate to the activation page
+      context.pushNamed('Home');
+    } else if (response.statusCode == 400) {
+      _showErrorPopup(context, 'Invalid input arguments');
+    } else if (response.statusCode == 500) {
+      _showErrorPopup(context, 'Something went wrong..');
+    }
+  }
+
+  void _showErrorPopup(BuildContext context, String message) {
+    // Show a popup with the error message
+    showDialog(
+      context: context,
+      builder: (BuildContext context) {
+        return AlertDialog(
+          backgroundColor: Colors.red,
+          content: Text(
+            message,
+            style: TextStyle(color: Colors.white),
+          ),
+          actions: <Widget>[
+            TextButton(
+              child: Text('OK', style: TextStyle(color: Colors.white)),
+              onPressed: () {
+                Navigator.of(context).pop();
+              },
+            ),
+          ],
+        );
+      },
+    );
+  }
 
   @override
   void initState(BuildContext context) {
